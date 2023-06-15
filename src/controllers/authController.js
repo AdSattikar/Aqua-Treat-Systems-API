6d91edc4d9f9c40d46021e16a9913a065afae60e
Adnan Sattikar <adnan.sattikar2001@gmail.com>
2023-06-15 19:01:54 +0530

    added CORS package

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,6 +7,7 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 // Authenticate an admin
 exports.loginAdmin = async (req, res) => {
+  let success= false
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
@@ -16,12 +17,12 @@ exports.loginAdmin = async (req, res) => {
   try {
     const admin = await Admin.findOne({ email });
     if (!admin) {
-      return res.status(400).json({ error: 'Please try to login with correct credentials' });
+      return res.status(400).json({ success, error: 'Please try to login with correct credentials' });
     }
 
     const passwordCompare = await bcrypt.compare(password, admin.password);
     if (!passwordCompare) {
-      return res.status(400).json({ error: 'Please try to login with correct credentials' });
+      return res.status(400).json({ success, error: 'Please try to login with correct credentials' });
     }
 
     const data = {
@@ -30,8 +31,9 @@ exports.loginAdmin = async (req, res) => {
       }
     };
     const authtoken = jwt.sign(data, JWT_SECRET);
+    success= true;
 
-    res.json({ authtoken });
+    res.json({success, authtoken });
   } catch (error) {
     console.error(error.message);
     res.status(500).send('Internal Server Error');
--- src/index.js
+++ src/index.js
@@ -2,22 +2,23 @@ const express = require('express')
 
 require("./db/databaseConnection")
 
+const cors = require('cors')
+
 const customerRouter = require('./routes/customer')
 const adminRouter = require('./routes/admin')
 const loginRouter = require('./routes/login')
 
 const app = express();
+
 const port = process.env.PORT || 3000
 
-
-app.get('/',(req,res) =>{
-    res.send("tested get request")
-})
-
 app.use(express.json())
+app.use(cors())
 
+//Routers
 app.use(customerRouter)
 app.use(adminRouter)
 app.use(loginRouter)
 
+//Server Running 
 app.listen(port, () => console.log(`Server running at port ${port}`));
